61b95f871c8ff70b08247c1ae72203d1515b845e
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-03 16:43:56 -0600

    se agrego carpeta data

---
+++ data/todoDummyData.jsx
@@ -0,0 +1,32 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Sacar la ",
+    isCompleted: false,
+    isToday: true,
+    hour: "10:00",
+  },
+  {
+    id: 2,
+    text: "Ir al médico",
+    isCompleted: false,
+    isToday: true,
+    hour: "12:00",
+  },
+  {
+    id: 3,
+    text: "Pasear al perro",
+    isCompleted: true,
+    isToday: true,
+    hour: "19:00",
+  },
+  {
+    id: 4,
+    text: "Hacer mi tarea",
+    isCompleted: true,
+    isToday: false,
+    hour: "20:00",
+  },
+];
+
+module.exports = { todosData };
